8ccb8f3a410dda6bf437aaaf88174de118e2d844
unknown <jason.nares@xde.com.ph>
2019-06-14 10:34:21 +0800

    initial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Button as AntButton } from 'antd';
+import styled from 'styled-components'
 
+/**
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
+}
+**/
+
+
+
+class App extends Component {
+  render() {
+  
+    const StyledButton = styled.button`
+      background: transparent;
+      border-radius: 3px;
+      border: 2px solid palevioletred;
+      color: palevioletred;
+      margin: 0 1em;
+      padding: 0.25em 1em;
+    `
+    return (
+      <div>
+        <center>Sonson Ixon</center>
+        <StyledButton type="primary" icon="search" shape="round" size="large">Sample Button</StyledButton>
+      </div>
+    );
+  }
 }
 
 export default App;
